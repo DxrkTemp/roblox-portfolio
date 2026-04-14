ea6cf884c69b53aea884eae9b5dff70e9864ac90
DxrkTemp <jjhombid@mcm.edu.ph>
2026-04-14 16:14:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ elements.forEach(el => observer.observe(el));
 window.addEventListener("load", () => {
   document.querySelectorAll(".skill-bar").forEach(bar => {
     const fill = document.createElement("div");
-    fill.style.width = bar.dataset.skill || "50%";
+    fill.style.width = bar.dataset.skill;
     bar.appendChild(fill);
   });
 });
@@ -39,7 +39,6 @@ document.querySelectorAll(".video-card").forEach(card => {
   const video = card.querySelector("video");
 
   card.addEventListener("mouseenter", () => video.play());
-
   card.addEventListener("mouseleave", () => {
     video.pause();
     video.currentTime = 0;
@@ -50,44 +49,17 @@ const menuBtn = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
 menuBtn.addEventListener("click", () => {
+
   navLinks.classList.toggle("active");
+
 });
 
 document.querySelectorAll(".nav-links a").forEach(link => {
+
   link.addEventListener("click", () => {
+
     navLinks.classList.remove("active");
-  });
-});
 
-document.querySelectorAll(".card").forEach(card => {
-
-  card.addEventListener("mousemove", e => {
-    const rect = card.getBoundingClientRect();
-
-    const x = (e.clientX - rect.left) / rect.width;
-    const y = (e.clientY - rect.top) / rect.height;
-
-    const rotateX = (y - 0.5) * 12;
-    const rotateY = (x - 0.5) * -12;
-
-    card.style.transform = `
-      perspective(1000px)
-      rotateX(${rotateX}deg)
-      rotateY(${rotateY}deg)
-      scale(1.03)
-    `;
-
-    card.style.setProperty("--x", `${x * 100}%`);
-    card.style.setProperty("--y", `${y * 100}%`);
-  });
-
-  card.addEventListener("mouseleave", () => {
-    card.style.transform = `
-      perspective(1000px)
-      rotateX(0deg)
-      rotateY(0deg)
-      scale(1)
-    `;
   });
 
 });
